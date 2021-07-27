21640bece7a7d701591d804b64e199ff45406dee
Henrique Silva de Carvalho <henrique.silvac@outlook.com>
2021-07-27 17:34:10 -0300

    Custom subclasses of NSObject

--- Project 10/ViewController.swift
+++ Project 10/ViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    var people = [Person]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,6 +43,9 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         if let jpegData = image.jpegData(compressionQuality: 0.8) {
             try? jpegData.write(to: imagePath)
         }
+        let person = Person(name: "Unknowm", image: imageName)
+        people.append(person)
+        collectionView.reloadData()
         
         dismiss(animated: true)
     }
